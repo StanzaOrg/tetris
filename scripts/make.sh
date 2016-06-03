d57fde8a7e372501f4643c346fdeafac5fcb6a2f
Patrick S. Li <patrickli.2001@gmail.com>
2016-06-03 16:19:37 -0700

    Updated make script to build osx bundles.

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,9 +1,13 @@
 #Adjust these to point to your QT installation
 QTDIR=/Users/psli/Qt5.6.0
 
+stanza src/libqt.stanza src/tetris.stanza -s tetris.s
+
+mkdir -p build
 cd build
 $QTDIR/5.6/clang_64/bin/qmake ../src/tetris.pro -r -spec macx-clang
 make
+$QTDIR/5.6/clang_64/bin/macdeployqt tetris.app -dmg
 cd ..
 
-stanza src/libqt.stanza src/tetris.stanza -o tetris -ccfiles build/libtetris.a -ccflags "-lstdc++ -F$QTDIR/5.6/clang_64/lib -framework QtWidgets -framework QtGui -framework QtCore -framework OpenGL -framework AGL -Wl,-rpath,$QTDIR/5.6/clang_64/lib"
+
