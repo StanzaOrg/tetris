13838abd85a9bec2d3a3289e935bbd3f5993f0ce
Patrick S. Li <patrickli.2001@gmail.com>
2016-06-03 11:34:29 -0700

    Added autodelete pointers

--- scripts/make.sh
+++ scripts/make.sh
@@ -2,4 +2,4 @@ cd build
 /Users/psli/Qt5.6.0/5.6/clang_64/bin/qmake ../src/tetris.pro -r -spec macx-clang
 make
 cd ..
-stanza src/libqt.stanza src/tetris.stanza -o tetris -ccfiles build/libtetris.a -ccflags "-lstdc++ -F/Users/psli/Qt5.6.0/5.6/clang_64/lib -framework QtWidgets -framework QtGui -framework QtCore -framework OpenGL -framework AGL -Wl,-rpath,/Users/psli/Qt5.6.0/5.6/clang_64/lib"
+stanza src/libqt.stanza src/tetris.stanza -o tetris -ccfiles build/libtetris.a -ccflags "-lstdc++ -F/Users/psli/Qt5.6.0/5.6/clang_64/lib -framework QtWidgets -framework QtGui -framework QtCore -framework OpenGL -framework AGL -Wl,-rpath,/Users/psli/Qt5.6.0/5.6/clang_64/lib" -pkg-path ../stanza/testpkg
