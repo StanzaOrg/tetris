f6f048717285c88e4619f62669e89985e820497e
Patrick S. Li <patrickli.2001@gmail.com>
2016-06-03 15:57:53 -0700

    Introduced QTDIR variable in make script.

--- scripts/make.sh
+++ scripts/make.sh
@@ -1,5 +1,9 @@
+#Adjust these to point to your QT installation
+QTDIR=/Users/psli/Qt5.6.0
+
 cd build
-/Users/psli/Qt5.6.0/5.6/clang_64/bin/qmake ../src/tetris.pro -r -spec macx-clang
+$QTDIR/5.6/clang_64/bin/qmake ../src/tetris.pro -r -spec macx-clang
 make
 cd ..
-stanza src/libqt.stanza src/tetris.stanza -o tetris -ccfiles build/libtetris.a -ccflags "-lstdc++ -F/Users/psli/Qt5.6.0/5.6/clang_64/lib -framework QtWidgets -framework QtGui -framework QtCore -framework OpenGL -framework AGL -Wl,-rpath,/Users/psli/Qt5.6.0/5.6/clang_64/lib"
+
+stanza src/libqt.stanza src/tetris.stanza -o tetris -ccfiles build/libtetris.a -ccflags "-lstdc++ -F$QTDIR/5.6/clang_64/lib -framework QtWidgets -framework QtGui -framework QtCore -framework OpenGL -framework AGL -Wl,-rpath,$QTDIR/5.6/clang_64/lib"
